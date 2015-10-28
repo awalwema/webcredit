8b6b106c94202bfa7d72cd34b988030c6281d462
Andrew Walwema <awalwema@gmail.com>
2015-10-28 15:01:06 +0000

    Added 2 variables

--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request) :
+	price = 6 #simulating money
+	data = "Apples"
+
+	return render(request, 'index.html', {
+		'price': price,
+		'data': data,
+
+		})
--- webcredit/urls.py
+++ webcredit/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from collection import views
 
 urlpatterns = [
+	url(r'^$', views.index, name='home'),
+	url(r'^about/$',
+		TemplateView.as_view(template_name='about.html'),
+		name = 'about'),
+	url(r'^contact/$',
+		TemplateView.as_view(template_name='contact.html'),
+		name = 'contact'),
     url(r'^admin/', include(admin.site.urls)),
+
 ]
